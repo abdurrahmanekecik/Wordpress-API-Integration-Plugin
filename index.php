33182a0a63d14b737a9023c9e9bbf6057372d4de
Abdurrahman Ekecik <abdurrahmanekecik@gmail.com>
2022-09-24 22:53:24 +0300

    Table Create And Forms Add

--- index.php
+++ index.php
@@ -17,11 +17,7 @@ License: GNU
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-if (current_user_can('upload_files')) { 
-
-
-
-}
+wp_enqueue_style( 'api_integration_style', plugins_url( 'css/style.css', __FILE__ ), '', '1.0' );
 
 
 
@@ -33,10 +29,69 @@ add_menu_page('Api Integration', 'Api Integration', 'manage_options', 'api-integ
 
 
 
-function api_integration_function() { echo 'Merhabalar. Bu yeni eklentimiz.'; }
+function api_integration_function() { 
 
 
-wp_enqueue_style( 'api_integration_style', plugins_url( 'css/style.css', __FILE__ ), '', '1.0' );
+
 
 
 ?>
+<h1>Wordpress Apı Integration Plugin</h1>
+<br><br><br><br>
+<form method="POST">
+Google:<input type="text" name="google"><br>
+Yandex:<input type="text" name="yandex"><br>
+Live Support:<input type="text" name="live_support">
+<button type="submit">Submit</button>
+</form>
+
+
+<?php }
+
+
+
+
+
+function table_create() {
+
+
+$google=$_POST['google'];
+$yandex=$_POST['yandex'];
+$live_support=$_POST['live_support'];
+
+
+	global  $wpdb;
+	$charset =$wpdb -> get_charset_collate();
+	$table_name = $wpdb->prefix."api_integration";
+
+
+	$sql = "CREATE TABLE $table_name(
+		id int(11) NOT NULL AUTO_INCREMENT,
+		google VARCHAR(255) NOT NULL,
+		yandex VARCHAR(255) NOT NULL,
+		live_support VARCHAR(255) NOT NULL,
+		UNIQUE KEY id(id)) $charset;";
+
+
+	require_once(ABSPATH. "wp-admin/includes/upgrade.php");
+	dbDelta($sql);
+
+
+register_activation_hook(__FILE__, 'creating_plugin_table');
+
+$wpdb->insert($table_name, array(
+
+"google"		=> $_POST['google'],
+"yandex"		=> $_POST['yandex'],
+"live_support"  => $_POST['live_support'],
+
+));
+
+
+
+
+
+}
+table_create();
+
+ ?>
